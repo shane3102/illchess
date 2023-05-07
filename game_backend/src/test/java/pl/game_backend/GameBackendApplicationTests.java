2a10bfca15e7ee379e841752390f837175bf32a9
shane3102 <bkucharczyk2000@gmail.com>
2023-05-07 20:40:00 +0200

    add game backend (ws + redis) and docker compose file initializing redis container

---
+++ game_backend/src/test/java/pl/game_backend/GameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.game_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
