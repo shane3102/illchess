e7ca40bebb64a265dda87c0c746bdd745b2c7656
shane3102 <bkucharczyk2000@gmail.com>
2023-07-13 20:51:52 +0200

    add simple redis configuration

--- web-socket/illchess-server/src/main/java/pl/illchess/websocket/server/IllChessWebSocketApplication.java
+++ web-socket/illchess-server/src/main/java/pl/illchess/websocket/server/IllChessWebSocketApplication.java
@@ -3,8 +3,10 @@ package pl.illchess.websocket.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "pl.illchess")
 public class IllChessWebSocketApplication {
+
+	//@EnableJpaRepositories, @EntityScan, @Configuration + @ComponentScan + @Bean (nad metodą)
 
 	public static void main(String[] args) {
 		SpringApplication.run(IllChessWebSocketApplication.class, args);
---
+++ web-socket/illchess-server/src/main/java/pl/illchess/websocket/server/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package pl.illchess.websocket.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${redis.hostname}")
+    private String hostname;
+
+    @Value("${redis.port}")
+    private int port;
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(hostname);
+        configuration.setPort(port);
+
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+        return template;
+    }
+}
