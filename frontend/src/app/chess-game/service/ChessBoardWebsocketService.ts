45c4ba184ce3b21dddb22051ff7f6a88181046b7
shane3102 <bkucharczyk2000@gmail.com>
2024-04-03 17:40:47 +0200

    remove useless stuff

--- frontend/src/app/chess-game/service/ChessBoardWebsocketService.ts
+++ frontend/src/app/chess-game/service/ChessBoardWebsocketService.ts
@@ -11,7 +11,7 @@ export class ChessBoardWebsocketService {
     async subscribe(topic: string, callback: any): Promise<Subscription> {
         const rxStomp = new RxStomp();
         rxStomp.configure({
-            brokerURL: 'ws://192.168.1.49:8080/chess',
+            brokerURL: 'ws://localhost:8080/chess',
         });
 
         rxStomp.activate();
