2cab048cfa7d5d0251c7b32ecf9ef89e156890f3
shane3102 <bkucharczyk2000@chessers>
2024-06-26 12:23:41 +0200

    connect new taking back move use cases to api, remove old, unused taking back move use case

---
+++ illchess-web-socket-service/illchess/illchess-adapter/src/main/java/pl/illchess/adapter/board/command/in/websocket/dto/AcceptTakingBackMoveRequest.java
@@ -0,0 +1,17 @@
+package pl.illchess.adapter.board.command.in.websocket.dto;
+
+import pl.illchess.application.board.command.in.AcceptTakingBackLastMoveUseCase.AcceptTakingBackMoveCmd;
+
+import java.util.UUID;
+
+public record AcceptTakingBackMoveRequest(
+    UUID boardId,
+    String username
+) {
+    public AcceptTakingBackMoveCmd toCmd() {
+        return new AcceptTakingBackMoveCmd(
+            boardId,
+            username
+        );
+    }
+}
---
+++ illchess-web-socket-service/illchess/illchess-adapter/src/main/java/pl/illchess/adapter/board/command/in/websocket/dto/RejectTakingBackMoveRequest.java
@@ -0,0 +1,17 @@
+package pl.illchess.adapter.board.command.in.websocket.dto;
+
+import pl.illchess.application.board.command.in.RejectTakingBackLastMoveUseCase.RejectTakingBackMoveCmd;
+
+import java.util.UUID;
+
+public record RejectTakingBackMoveRequest(
+    UUID boardId,
+    String username
+) {
+    public RejectTakingBackMoveCmd toCmd() {
+        return new RejectTakingBackMoveCmd(
+            boardId,
+            username
+        );
+    }
+}
