2d345199608a4c99b90e0e90d44643a4a52bacbf
shane3102 <bkucharczyk2000@chessers>
2024-07-09 20:56:48 +0200

    add more test cases

--- illchess-web-socket-service/illchess/illchess-application/src/test/groovy/pl/illchess/application/board/command/in/MovementOnPreMoveTest.groovy
+++ illchess-web-socket-service/illchess/illchess-application/src/test/groovy/pl/illchess/application/board/command/in/MovementOnPreMoveTest.groovy
@@ -57,12 +57,24 @@ class MovementOnPreMoveTest extends BoardSpecification {
 
         where:
         startPosition                                   | expectedEndingPosition
-        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w" | "rnbqkbnr/ppp5/3ppppp/8/2PP4/5NP1/PP2PPBP/RNBQK2R w"
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w" | "rnbqkbnr/ppp5/3ppppp/8/2PP4/5NP1/PP2PPBP/RNBQ1RK1 b"
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w" | "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b"
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w" | "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b"
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w" | "r1bqkb1r/pppp1ppp/5n2/4n3/4P3/2N5/PPPP1PPP/R1BQKB1R w"
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w" | "rnbqkb1r/p2ppppp/5n2/1PpP4/8/8/PP2PPPP/RNBQKBNR b"
         __
-        preMovesToPerform                                                                | _
-        [of("D2", "D4"), of("C2", "C4"), of("G1", "F3"), of("G2", "G3"), of("F1", "G2")] | _
+        preMovesToPerform                                                                                | _
+        [of("D2", "D4"), of("C2", "C4"), of("G1", "F3"), of("G2", "G3"), of("F1", "G2"), of("E1", "G1")] | _
+        [of("E2", "E4"), of("G1", "F3"), of("F1", "B5")]                                                 | _
+        [of("E2", "E4"), of("G1", "F3"), of("F1", "C4")]                                                 | _
+        [of("E2", "E4"), of("G1", "F3"), of("F3", "E5"), of("B1", "C3")]                                 | _
+        [of("D2", "D4"), of("C2", "C4"), of("C4", "B5"), of("D4", "D5")]                                 | _
         __
         movesFollowingPreMoves                                                           | _
         [of("E7", "E6"), of("F7", "F6"), of("G7", "G6"), of("H7", "H6"), of("D7", "D6")] | _
+        [of("E7", "E5"), of("B8", "C6")]                                                 | _
+        [of("E7", "E5"), of("B8", "C6")]                                                 | _
+        [of("E7", "E5"), of("B8", "C6"), of("G8", "F6"), of("C6", "E5")]                 | _
+        [of("C7", "C5"), of("B7", "B5"), of("G8", "F6")]                                 | _
     }
 }
