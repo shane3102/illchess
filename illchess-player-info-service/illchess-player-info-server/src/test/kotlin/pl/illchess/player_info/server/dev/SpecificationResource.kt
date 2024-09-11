2bf63d97bf3b3b008ba4e320fce0cad2a5755186
bartlomiej.kucharczy <bartlomiej.kucharczyk@envelo.pl>
2024-09-11 12:39:04 +0200

    Add rabbit test container to integration test

--- illchess-player-info-service/illchess-player-info-server/src/test/kotlin/pl/illchess/player_info/server/dev/SpecificationResource.kt
+++ illchess-player-info-service/illchess-player-info-server/src/test/kotlin/pl/illchess/player_info/server/dev/SpecificationResource.kt
@@ -2,6 +2,7 @@ package pl.illchess.player_info.server.dev
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager
 import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.containers.RabbitMQContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
@@ -16,21 +17,32 @@ open class SpecificationResource : QuarkusTestResourceLifecycleManager {
             .withDatabaseName("player_info_db")
             .withUsername("admin")
             .withPassword("admin")
+
+        @Container
+        private val RABBIT_MQ_CONTAINER = RabbitMQContainer("rabbitmq:3")
     }
 
     override fun start(): MutableMap<String, String> {
 
+        RABBIT_MQ_CONTAINER.start()
         MY_SQL_CONTAINER.start()
 
         val conf = HashMap<String, String>()
         conf["quarkus.datasource.jdbc.url"] = MY_SQL_CONTAINER.jdbcUrl
         conf["quarkus.datasource.username"] = MY_SQL_CONTAINER.username
         conf["quarkus.datasource.password"] = MY_SQL_CONTAINER.password
+
+        conf["rabbitmq-host"] = RABBIT_MQ_CONTAINER.host
+        conf["rabbitmq-port"] = RABBIT_MQ_CONTAINER.amqpPort.toString()
+        conf["rabbitmq-username"] = "guest"
+        conf["rabbitmq-password"] = "guest"
+
         return conf
     }
 
     override fun stop() {
         MY_SQL_CONTAINER.stop()
+        RABBIT_MQ_CONTAINER.stop()
     }
 
 }
