b728c1bc3961e43674347dfa46a8ef8ad67ebaea
bartlomiej.kucharczy <bartlomiej.kucharczyk@envelo.pl>
2024-09-20 09:46:44 +0200

    remove useless info passed by frontend, fix compose pass for redis db

--- frontend/src/app/chess-game/components/chess-promoting-piece/chess-promoting-piece.component.ts
+++ frontend/src/app/chess-game/components/chess-promoting-piece/chess-promoting-piece.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Piece, PieceColor, PieceInfo } from '../../model/PieceInfo';
-import { SquareInfo } from '../../model/SquareInfo';
-import { PieceDraggedInfo } from '../../model/PieceDraggedInfo';
 import { MovePieceRequest } from '../../model/MovePieceRequest';
+import { PieceDraggedInfo } from '../../model/PieceDraggedInfo';
+import { Piece, PieceColor } from '../../model/PieceInfo';
+import { SquareInfo } from '../../model/SquareInfo';
 
 @Component({
   selector: 'app-chess-promoting-piece',
@@ -26,8 +26,6 @@ export class ChessPromotingPieceComponent {
         'boardId': this.boardId,
         'startSquare': this.draggedPieceInfo.squareInfo.file + this.draggedPieceInfo.squareInfo.rank,
         'targetSquare': this.squareInfo.file + this.squareInfo.rank,
-        'pieceColor': this.draggedPieceInfo.pieceInfo.color,
-        'pieceType': this.draggedPieceInfo.pieceInfo.type,
         'pawnPromotedToPieceType': piece,
         'username': this.username
       }
