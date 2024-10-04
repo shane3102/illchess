81b6e27d1ebdab43c2e46191ba4ad1dae2546d80
bartlomiej.kucharczy <bartlomiej.kucharczyk@envelo.pl>
2024-10-04 14:09:32 +0200

    Add rabbitmq container to test specification

--- illchess-web-socket-service/illchess-server/src/test/groovy/pl/illchess/SpecificationIT.groovy
+++ illchess-web-socket-service/illchess-server/src/test/groovy/pl/illchess/SpecificationIT.groovy
@@ -1,10 +1,12 @@
 package pl.illchess
 
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.DynamicPropertyRegistry
@@ -12,6 +14,7 @@ import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.RabbitMQContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.spock.Testcontainers
 import org.testcontainers.utility.DockerImageName
@@ -27,19 +30,33 @@ import spock.lang.Specification
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 abstract class SpecificationIT extends Specification {
 
+    private static final def REDIS_PORT = 6379
+    private static final def RABBITMQ_PORT = 5672
+
     @Autowired
     protected MockMvc mockMvc
 
+    @SpyBean
+    protected RabbitTemplate rabbitTemplate
+
     def setupSpec() {
         redis.start()
+        rabbitMq.start()
     }
 
     @DynamicPropertySource
     static void configure(DynamicPropertyRegistry registry) {
-        registry.add("redis.hostname", () -> "gamedb-test")
-        registry.add("redis.port", () -> "6379")
+        registry.add("redis.hostname", () -> redis.host)
+        registry.add("redis.port", () -> redis.getMappedPort(REDIS_PORT).toString())
+        registry.add("spring.rabbitmq.host", () -> rabbitMq.host)
+        registry.add("spring.rabbitmq.port", () -> rabbitMq.getMappedPort(RABBITMQ_PORT).toString())
+        registry.add("spring.rabbitmq.username", () -> "guest")
+        registry.add("spring.rabbitmq.password", () -> "guest")
     }
 
     @Container
-    static redis = new GenericContainer<>(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(6379)
+    static redis = new GenericContainer<>(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(REDIS_PORT)
+
+    @Container
+    static rabbitMq = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3-management"))
 }
