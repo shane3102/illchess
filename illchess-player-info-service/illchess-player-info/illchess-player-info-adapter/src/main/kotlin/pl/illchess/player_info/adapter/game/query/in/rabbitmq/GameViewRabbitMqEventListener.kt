c729ad541832a23a423f2ec686709973a30d4525
shane3102 <bkucharczyk2000@gmail.com>
2024-10-28 18:28:27 +0100

    Change simplest uuid to object with uuid of unsuccessful game obtain

--- illchess-player-info-service/illchess-player-info/illchess-player-info-adapter/src/main/kotlin/pl/illchess/player_info/adapter/game/query/in/rabbitmq/GameViewRabbitMqEventListener.kt
+++ illchess-player-info-service/illchess-player-info/illchess-player-info-adapter/src/main/kotlin/pl/illchess/player_info/adapter/game/query/in/rabbitmq/GameViewRabbitMqEventListener.kt
@@ -1,14 +1,14 @@
 package pl.illchess.player_info.adapter.game.query.`in`.rabbitmq
 
+import pl.illchess.player_info.application.game.query.out.model.GameErrorObtainingView
 import pl.illchess.player_info.application.game.query.out.model.GameView
 import pl.illchess.player_info.domain.game.event.ErrorWhileSavingGameEvent
 import pl.illchess.player_info.domain.game.event.GameSavedEvent
-import java.util.UUID
 
 interface GameViewRabbitMqEventListener {
 
     fun sendGameViewOnGameSaved(gameSavedEvent: GameSavedEvent): GameView
 
-    fun sendErrorInfoOnErrorWhileSavingGame(errorWhileSavingGameEvent: ErrorWhileSavingGameEvent): UUID
+    fun sendErrorInfoOnErrorWhileSavingGame(errorWhileSavingGameEvent: ErrorWhileSavingGameEvent): GameErrorObtainingView
 
 }
---
+++ illchess-player-info-service/illchess-player-info/illchess-player-info-application/src/main/kotlin/pl/illchess/player_info/application/game/query/out/model/GameErrorObtainingView.kt
@@ -0,0 +1,5 @@
+package pl.illchess.player_info.application.game.query.out.model
+
+import java.util.UUID
+
+data class GameErrorObtainingView(val id: UUID)
