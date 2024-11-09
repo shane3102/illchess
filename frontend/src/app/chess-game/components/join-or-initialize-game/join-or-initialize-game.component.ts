fa4df131d088af9cee096b56ebc0ffc6523f62bd
shane3102 <bkucharczyk2000@gmail.com>
2024-11-09 17:52:14 +0100

    implement and minor refactor to join or initialize game component, make horizontal and vertical line in additional in fo non-resizable

--- frontend/src/app/chess-game/components/join-or-initialize-game/join-or-initialize-game.component.ts
+++ frontend/src/app/chess-game/components/join-or-initialize-game/join-or-initialize-game.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ChessGameState } from '../../../shared/state/chess-game.state';
-import { initializeBoard } from '../../../shared/state/board/board.actions';
+import { initializeBoard, refreshBoard } from '../../../shared/state/board/board.actions';
 import { InitializeBoardRequest } from '../../../shared/model/game/InitializeBoardRequest';
 import { initializedBoardIdSelector } from '../../../shared/state/board/board.selectors';
 import { Router } from '@angular/router';
 import { RefreshBoardDto } from '../../../shared/model/game/RefreshBoardRequest';
+import { takeUntil, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-join-or-initialize-game',
@@ -14,15 +15,13 @@ import { RefreshBoardDto } from '../../../shared/model/game/RefreshBoardRequest'
 })
 export class JoinOrInitializeGameComponent implements OnInit {
 
+  boardId: string
   randomNames: string[] = ["Mark", "Tom", "Pablo", "Jose", "William"]
 
   username: string = this.randomNames[Math.floor(Math.random() * this.randomNames.length)] + Math.floor(100 * Math.random())
 
-  constructor(
-    private store: Store<ChessGameState>,
-    private router: Router
-  ) {
-  }
+  private store = inject(Store<ChessGameState>)
+  private router = inject(Router)
 
   ngOnInit(): void {
     let initializeNewBoardRequest: InitializeBoardRequest = {
@@ -30,11 +29,17 @@ export class JoinOrInitializeGameComponent implements OnInit {
     }
     this.store.dispatch(initializeBoard(initializeNewBoardRequest))
 
-    this.store.select(initializedBoardIdSelector).subscribe(
-      (dto: RefreshBoardDto) => {
-        this.router.navigateByUrl(`/game/${dto.boardId}/${this.username}`)
-      }
-    )
+    this.store.select(initializedBoardIdSelector)
+      .pipe(takeWhile(() => !this.boardId))
+      .subscribe(
+        (dto: RefreshBoardDto) => {
+          if (dto.boardId) {
+            this.store.dispatch(refreshBoard(dto))
+            this.router.navigateByUrl(`/game/${dto.boardId}/${this.username}`)
+            this.boardId = dto.boardId
+          }
+        }
+      )
   }
 
 }
