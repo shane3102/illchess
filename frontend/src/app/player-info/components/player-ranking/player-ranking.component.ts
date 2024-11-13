15739635135b2563495bb56ffc9ab02abfc43795
bartlomiej.kucharczy <bartlomiej.kucharczyk@envelo.pl>
2024-11-13 12:03:09 +0100

    get rid of duplicated ws connection

--- frontend/src/app/player-info/components/player-ranking/player-ranking.component.ts
+++ frontend/src/app/player-info/components/player-ranking/player-ranking.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, Subject, Subscription, combineLatest, map } from 'rxjs';
+import { Observable, Subject, combineLatest, map } from 'rxjs';
 import { PlayerView } from 'src/app/shared/model/player-info/PlayerView';
 import { ChessGameState } from 'src/app/shared/state/chess-game.state';
 import { loadPlayerRanking } from 'src/app/shared/state/player-info/player-info.actions';
 import { commonPageSize, pageNumberPlayerRankingSelector, playerRankingSelector, totalPageNumberPlayerRankingSelector } from 'src/app/shared/state/player-info/player-info.selectors';
 import { PageData } from '../../model/PageData';
-import { ChessBoardWebsocketService } from 'src/app/shared/service/GameWebsocketService';
 
 @Component({
   selector: 'app-player-ranking',
   templateUrl: './player-ranking.component.html',
   styleUrls: ['./player-ranking.component.scss']
 })
-export class PlayerRankingComponent implements OnInit, OnDestroy {
+export class PlayerRankingComponent implements OnInit {
+
+  @Input() reloadPage$: Observable<void>
 
   store = inject(Store<ChessGameState>)
-  chessBoardWebSocketService = inject(ChessBoardWebsocketService)
 
   data$: Observable<PageData>
   reloadSubject: Subject<void> = new Subject<void>();
@@ -25,8 +25,6 @@ export class PlayerRankingComponent implements OnInit, OnDestroy {
   currentPage$: Observable<number> = this.store.select(pageNumberPlayerRankingSelector)
   totalPages$: Observable<number | null | undefined> = this.store.select(totalPageNumberPlayerRankingSelector)
   commonPageSize$: Observable<number> = this.store.select(commonPageSize)
-
-  private obtainStatusSubscription$: Subscription
 
   ngOnInit(): void {
     this.data$ = combineLatest([this.currentPage$, this.commonPageSize$])
@@ -40,20 +38,7 @@ export class PlayerRankingComponent implements OnInit, OnDestroy {
           }
         )
       )
-    
-    setTimeout(
-      async () => {
-        this.obtainStatusSubscription$ = await this.chessBoardWebSocketService.subscribe(
-          `/chess-topic/obtain-status`,
-          () => this.reloadSubject.next()
-        )
-      }
-    )
-    
-  }
 
-  ngOnDestroy(): void {
-    this.obtainStatusSubscription$.unsubscribe()
   }
 
   reloadRanking(event: { pageNumber: number, pageSize: number }) {
