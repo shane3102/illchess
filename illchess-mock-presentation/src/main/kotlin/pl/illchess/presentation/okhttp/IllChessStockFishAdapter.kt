2be8a54a172ef53898d975a00796275c71a84666
bartlomiej.kucharczy <bartlomiej.kucharczyk@envelo.pl>
2024-11-13 11:35:22 +0100

    hack fix for waiting too long for stockfish best moves

--- illchess-mock-presentation/src/main/kotlin/pl/illchess/presentation/okhttp/IllChessStockFishAdapter.kt
+++ illchess-mock-presentation/src/main/kotlin/pl/illchess/presentation/okhttp/IllChessStockFishAdapter.kt
@@ -3,32 +3,40 @@ package pl.illchess.presentation.okhttp
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.net.SocketTimeoutException
+import java.util.Properties
+import java.util.UUID
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import pl.illchess.presentation.properties.PropertiesLoader
-import java.util.*
 
 class IllChessStockFishAdapter(
     private val properties: Properties = PropertiesLoader.loadProperties(),
     private val okHttpClient: OkHttpClient = OkHttpClient(),
-    private val objectMapper: ObjectMapper = jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    private val objectMapper: ObjectMapper = jacksonObjectMapper().configure(
+        DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+        false
+    )
 ) {
 
     fun loadTopMoves(boardId: UUID, moveCount: Int): TopMoves? {
-        val illChessStockfishUrl = properties.getProperty("illchess.stockfish.url")
-        val request: Request = Request.Builder()
-            .url("$illChessStockfishUrl/api/board/top-moves/$moveCount/$boardId")
-            .build()
-        val call = okHttpClient.newCall(request)
-        val response = call.execute()
+        try {
+            val illChessStockfishUrl = properties.getProperty("illchess.stockfish.url")
+            val request: Request = Request.Builder()
+                .url("$illChessStockfishUrl/api/board/top-moves/$moveCount/$boardId")
+                .build()
+            val call = okHttpClient.newCall(request)
+            val response = call.execute()
 
-        val bestMoves = response.body?.string()
+            val bestMoves = response.body?.string()
 
-        val fenBoardPosition: TopMoves? =
-            if (response.body == null) null
-            else objectMapper.readValue(bestMoves, TopMoves::class.java)
-
-        return fenBoardPosition
+            return if (response.body == null) null else objectMapper.readValue(
+                bestMoves,
+                TopMoves::class.java
+            )
+        } catch (e: SocketTimeoutException) {
+            return loadTopMoves(boardId, moveCount)
+        }
     }
 
     data class TopMoves(val topMoves: List<String>)
