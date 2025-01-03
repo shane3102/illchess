c381d4bf22ff380e08a7dd5a6f85d205d9ec07ab
shane3102 <bkucharczyk2000@gmail.com>
2025-01-03 20:56:22 +0100

    add ui for adding and removing bots

--- illchess-frontend/src/app/bot/components/bot-managment/bot-managment.component.ts
+++ illchess-frontend/src/app/bot/components/bot-managment/bot-managment.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { BotView } from 'src/app/shared/model/stockfish/BotView';
-import { addBots, deleteBots } from 'src/app/shared/state/bot/bot.actions';
+import { addBots, deleteBots, loadCurrentllyRunBots } from 'src/app/shared/state/bot/bot.actions';
 import { botListSelector, botManagmentShown } from 'src/app/shared/state/bot/bot.selectors';
 import { ChessGameState } from 'src/app/shared/state/chess-game.state';
 
@@ -13,19 +14,21 @@ import { ChessGameState } from 'src/app/shared/state/chess-game.state';
 })
 export class BotManagmentComponent {
 
+  plusIcon = faPlus
+  minusIcon = faMinus
+
   store = inject(Store<ChessGameState>)
   bots$: Observable<BotView[] | undefined | null> = this.store.select(botListSelector)
   botManagmentShown$: Observable<boolean | undefined | null> = this.store.select(botManagmentShown)
 
   added: string
-  deleted: string
 
   addBot() {
     this.store.dispatch(addBots({username: this.added}))
   }
 
-  deleteBot() {
-    this.store.dispatch(deleteBots({username: this.deleted}))
+  deleteBot(username: string) {
+    this.store.dispatch(deleteBots({username: username}))
   }
 
 
