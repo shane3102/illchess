d4bf45364431ac8d100f305297388ed7b3400961
shane3102 <bkucharczyk2000@gmail.com>
2025-01-04 22:42:29 +0100

    remove full mock presentation module (functionality absorbed by stockfish service)

--- illchess-mock-presentation/src/main/kotlin/pl/illchess/presentation/exception/BoardNotFound.kt
+++
@@ -1,7 +0,0 @@
-package pl.illchess.presentation.exception
-
-import java.util.UUID
-
-class BoardNotFound(boardId: UUID) : RuntimeException(
-    "Board with id = %s not found. Assuming game finished and joining next game".format(boardId)
-)
