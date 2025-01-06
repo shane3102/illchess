d5d1528a0ca6babf6b057a38e3a27d11409f9b93
shane3102 <bkucharczyk2000@gmail.com>
2025-01-06 14:08:17 +0100

    use not-this-much recognisable random names

--- illchess-frontend/src/app/shared/service/GenerateRandomNameService.ts
+++ illchess-frontend/src/app/shared/service/GenerateRandomNameService.ts
@@ -4,7 +4,7 @@ import { Injectable } from "@angular/core";
     providedIn: 'root'
 })
 export class GenerateRandomNameService {
-    randomNames: string[] = ["Valerie", "Vincent", "Ciri", "Geralt", "Judy", "Yennefer", "Case", "Roach", "Johnny", "Eren", "Levi", "Jinx", "Ekko"]
+    randomNames: string[] = ["Valerie", "Vincent", "James", "Michael", "Patricia", "Robert", "Linda", "Johnny", "David", "Susan", "Sarah"]
 
     generateRandomName(): string { 
         return this.randomNames[Math.floor(Math.random() * this.randomNames.length)] + Math.floor(100 * Math.random())
