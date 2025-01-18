0cc3cfce7c4c395751fd1e40b465fd0f1dfee846
shane3102 <bucharczyk2000@gmail.com>
2025-01-18 22:56:10 +0100

    make spinner resizable

--- illchess-frontend/src/app/shared/components/spinner/spinner.component.ts
+++ illchess-frontend/src/app/shared/components/spinner/spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent {
+
+  @Input() size: number = 10
+  ringSize: number
+  marginAndBorderSize: number
+
+  ngOnInit(): void {
+    this.ringSize = this.size * 4/5
+    this.marginAndBorderSize = this.size * 1/10
+  }
 
 }
