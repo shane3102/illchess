b680914b522a1341aabcfb8120d01f1f462df1a5
shane3102 <bkucharczyk2000@gmail.com>
2025-03-22 12:58:22 +0100

    fix mongodb adapters
    Signed-off-by: shane3102 <bkucharczyk2000@gmail.com>

--- illchess-stockfish-service/illchess-stockfish/illchess-stockfish-adapter/src/main/kotlin/pl/illchess/stockfish/adapter/evaluation/command/out/mongodb/mapper/BestMoveAndEvaluationMapper.kt
+++ illchess-stockfish-service/illchess-stockfish/illchess-stockfish-adapter/src/main/kotlin/pl/illchess/stockfish/adapter/evaluation/command/out/mongodb/mapper/BestMoveAndEvaluationMapper.kt
@@ -1,5 +1,6 @@
 package pl.illchess.stockfish.adapter.evaluation.command.out.mongodb.mapper
 
+import org.bson.types.ObjectId
 import pl.illchess.stockfish.adapter.evaluation.command.out.mongodb.model.BestMoveAndContinuationEntity
 import pl.illchess.stockfish.domain.board.domain.EvaluationBoardInformation
 import pl.illchess.stockfish.domain.evaluation.domain.BestMoveAndContinuation
@@ -8,8 +9,10 @@ class BestMoveAndEvaluationMapper {
     companion object {
         fun toEntity(
             evaluationBoardInformation: EvaluationBoardInformation,
-            bestMoveAndContinuation: BestMoveAndContinuation
+            bestMoveAndContinuation: BestMoveAndContinuation,
+            id: ObjectId? = null
         ) = BestMoveAndContinuationEntity(
+            id ?: ObjectId.get(),
             EvaluationBoardInformationMapper.toEntity(evaluationBoardInformation),
             bestMoveAndContinuation.bestMove,
             bestMoveAndContinuation.continuation
--- illchess-stockfish-service/illchess-stockfish/illchess-stockfish-adapter/src/main/kotlin/pl/illchess/stockfish/adapter/evaluation/command/out/mongodb/model/BestMoveAndContinuationEntity.kt
+++ illchess-stockfish-service/illchess-stockfish/illchess-stockfish-adapter/src/main/kotlin/pl/illchess/stockfish/adapter/evaluation/command/out/mongodb/model/BestMoveAndContinuationEntity.kt
@@ -1,12 +1,20 @@
 package pl.illchess.stockfish.adapter.evaluation.command.out.mongodb.model
 
 import io.quarkus.mongodb.panache.common.MongoEntity
+import org.bson.codecs.pojo.annotations.BsonCreator
 import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.codecs.pojo.annotations.BsonProperty
+import org.bson.types.ObjectId
 
 @MongoEntity(collection = "BOARD_BEST_MOVE_AND_CONTINUATION")
-data class BestMoveAndContinuationEntity(
+data class BestMoveAndContinuationEntity @BsonCreator constructor(
     @BsonId
+    @BsonProperty("_id")
+    val id: ObjectId,
+    @BsonProperty("evaluationBoardInformation")
     val evaluationBoardInformation: EvaluationBoardInformationEntity,
+    @BsonProperty("bestMove")
     val bestMove: String,
+    @BsonProperty("continuation")
     val continuation: List<String>
 )
